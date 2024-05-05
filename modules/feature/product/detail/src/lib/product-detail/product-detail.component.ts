9d934d75433ed40a29fe67cf66fcdb908a5610e2
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2024-05-05 16:03:30 +0200

    feat(product-detail): integrates products api

--- modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Product, ProductSearchService } from '@ecommerce/product-data-access';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getParams } from './get-params';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'ecommerce-product-detail',
@@ -10,5 +12,9 @@ import { getParams } from './get-params';
   styleUrl: './product-detail.component.scss',
 })
 export class ProductDetailComponent {
-  id$ = getParams();
+  productSearchService = inject(ProductSearchService);
+
+  product$: Observable<Product> = getParams().pipe(
+    switchMap((id) => this.productSearchService.getById(id))
+  );
 }
