2e63525b400737862ebe3bb17cc8c483af319fa1
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2024-07-18 04:55:26 +0200

    feat(auth-form): integrates auth form

--- modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from '../auth-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ecommerce-auth-form-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './auth-form-password.component.html',
-  styleUrl: './auth-form-password.component.css',
+  styleUrl: './auth-form-password.component.scss',
 })
 export class AuthFormPasswordComponent {
   control = inject(AuthFormComponent).form.controls.password;
